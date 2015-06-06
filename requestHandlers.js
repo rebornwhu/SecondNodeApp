29c76e5650d07872a1b28ea210c6717db4118539
rebornwhu <rebornwhu@gmail.com>
2015-06-06 16:37:43 -0700

    Add post data

--- requestHandlers.js
+++ requestHandlers.js
@@ -5,9 +5,9 @@ function start(response) {
 
     var body = '<html>' +
             '<head>' +
-            '<meta http-eqiv="Content-Type" content="text/html; ' +
+            '<meta http-equiv="Content-Type" content="text/html; ' +
             'charset=UTF=8" />' +
-            '</head.' +
+            '</head>' +
             '<body>' +
             '<form action="/upload" method="post">'+
             '<textarea name="text" row="20" cols="60"></textarea>' +
@@ -19,21 +19,12 @@ function start(response) {
     response.writeHead(200, {"Content-Type": "text/html"});
     response.write(body);
     response.end();
-
-    /*exec("ls -lah", function (error, stdout, stderr) {
-        // exec("find /",
-        // 	{timeout: 10000, maxBuffer: 20000*1024},
-        // 	function (error, stdout, stderr) {
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write(stdout);
-        response.end();
-    });*/
 }
 
-function upload(response) {
+function upload(response, postData) {
     console.log("Request handler 'upload' was called.");
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello Upload");
+    response.write("You've sent: " + postData);
     response.end();
 }
 
